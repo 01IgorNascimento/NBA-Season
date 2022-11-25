d6fed35b28e8919fff4fd57a98de9455375425e7
01IgorNascimento <igor.silva@sptech.school>
2022-11-25 19:40:08 -0300

    Validacao refeita

--- dev/site/public/FuncoesSite/validaCadastro.js
+++ dev/site/public/FuncoesSite/validaCadastro.js
@@ -4,29 +4,21 @@ var permissaoConfirmaSenha = 0
 var permissaoNome = 0
 
 
-function validacaoEmail(field) {
-    usuario = field.value.substring(0, field.value.indexOf("@"));
-    dominio = field.value.substring(field.value.indexOf("@") + 1, field.value.length);
-
-    if ((usuario.length >= 1) &&
-        (dominio.length >= 3) &&
-        (usuario.search("@") == -1) &&
-        (dominio.search("@") == -1) &&
-        (usuario.search(" ") == -1) &&
-        (dominio.search(" ") == -1) &&
-        (dominio.search(".") != -1) &&
-        (dominio.indexOf(".") >= 1) &&
-        (dominio.lastIndexOf(".") < dominio.length - 1)) {
-
-        document.getElementById("msgemail").innerHTML = "";
+function validacaoEmail(campo) {
+    var valorEmail = campo.value
+    var arrayValorEmail = valorEmail.split('');
+    var arroba = arrayValorEmail.indexOf('@');
+    var ponto = arrayValorEmail.indexOf('.');
+    console.log("Ponto: " + ponto + "Arroba: " + arroba)
+    if (arroba != -1 && ponto != -1) {
         permissaoGmail = 1
-    } else if (field.value == "") {
-        document.getElementById("msgemail").innerHTML = "";
-
+        msgemail.innerHTML = ""
+    }
+    if (campo.value == "") {
+        msgemail.innerHTML = ""
     }
     else {
-        document.getElementById("msgemail").innerHTML = "<font color='red'>E-mail inválido </font>";
-        permissaoGmail = 0
+        msgemail.innerHTML = "<font color='red'>Digite um E-Mail válido</font>"
     }
 }
 
--- documentacao/banco de dados/scripts/selects.sql
+++ documentacao/banco de dados/scripts/selects.sql
@@ -6,7 +6,9 @@
 
 select usuario.nome as nomeUsuario, avg(jogador.overhall) as mediaOverhall, max(jogador.overhall) as maior, sum(jogador.salario) as salario, usuario.id
     from usuario join timeCriado on fkUSuario = usuario.id
-    join jogador on timeCriado.nomeJogador = jogador.nome group by usuario.nome order by mediaOverhall desc ; 
+    join jogador on timeCriado.nomeJogador = jogador.nome group by usuario.nome order by mediaOverhall desc;
+    
+    
     
 select usuario.nome as nome, avg(jogador.overhall) as mediaOverhall, max(jogador.overhall) as maior, sum(jogador.salario) as salario, usuario.id
     from usuario join timeCriado on fkUSuario = usuario.id
@@ -24,19 +26,29 @@ select usuario.nome as nome, avg(jogador.overhall) as mediaOverhall, max(jogador
     
 	select timePadrao.nome as nome, max(jogador.overhall) as maiorOverhall, avg(jogador.overhall) as mediaOverhall, sum(jogador.salario) as salario,   timePadrao.imagemLogo as img from timePadrao
     join jogador on jogador.fkTimePadrao = timePadrao.id group by fkTimePadrao order by mediaOverhall desc;
+    
+    
+    
 	
 select usuario.nome as nome, avg(jogador.overhall) as mediaOverhall, max(jogador.overhall) as maiorOverhall, sum(jogador.salario) as salario, usuario.id
     from usuario join timeCriado on fkUSuario = usuario.id
     join jogador on timeCriado.nomeJogador = jogador.nome where usuario.id=1000;
+    
+    
+    
       
 
 select usuario.nome as nomeUsuario, avg(jogador.overhall) as mediaOverhall, max(jogador.overhall) as maior, sum(jogador.salario) as salario, usuario.id
     from usuario join timeCriado on fkUSuario = usuario.id
     join jogador on timeCriado.nomeJogador = jogador.nome group by usuario.nome order by mediaOverhall desc; 
     
-    select usuario.nome as nomeUsuario,  avg(jogador.overhall) as mediaOverhall, max(jogador.overhall) as maior, sum(jogador.salario) as salario, usuario.id
-    from usuario join timeCriado on fkUSuario = usuario.id
-    join jogador on timeCriado.nomeJogador = jogador.nome  group by usuario.nome order by salario desc;
+    select * from timeCriado;
+    
+    select timeCriado.nomeJogador, jogador.overhall, usuario.nome from timeCriado join jogador on timeCriado.fkJogador = jogador.id join usuario on timeCriado.fkUsuario = usuario.id group by usuario.nome order by jogador.overhall desc  limit 1;
+
+
+select usuario.nome as nomeUsuario,  avg(jogador.overhall) as mediaOverhall,  max(jogador.overhall) as maior, sum(jogador.salario) as salario, usuario.id
+    from usuario join timeCriado on fkUSuario = usuario.id join jogador on timeCriado.nomeJogador = jogador.nome  group by usuario.nome order by salario desc;
 
     
     
